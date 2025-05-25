ad574f9c614068853526af6951697d06ebb54047
OleinikKI <rubykoffline@gmail.com>
2025-05-25 15:29:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCpp/ConsoleCpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	// Comment
+	// Some
+	/*
+	comment
+	more comment
+
+	*/
+	setlocale(LC_ALL, "RU");
+
+	std::cout << "All is okey!\n" << std::endl;
+	std::cout << "Привет!";
+
+	return 0;
+}
