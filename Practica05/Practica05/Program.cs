5558574a88a745d1c6ffebd3e34c7ca4ef218ca0
OleinikKI <rubykoffline@gmail.com>
2025-05-25 15:55:14 +0300

    Выполнен расчет значения F.

--- Practica05/Practica05/Program.cs
+++ Practica05/Practica05/Program.cs
@@ -10,9 +10,10 @@ namespace Practica05
     {
         static void Main(string[] args)
         {
-            Console.Write(&quot; Введите значения параметра x = &quot;);
+            Console.Write("Введите значения параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+            double F = Math.Sin(x) - 2*Math.Cos(x);
         }
     }
 }
